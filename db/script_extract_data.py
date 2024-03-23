7ca41ef1c22e70de5d02254b4b8a8872b58e9743
vyktron <vyktron@proton.me>
2024-03-23 15:48:12 +0100

    enfant techno

--- db/script_extract_data.py
+++ db/script_extract_data.py
@@ -22,24 +22,50 @@ class Extractor:
         and save it to a CSV file
         """
 
-        CSV_FILENAME = "dictionary.csv"
+        CSV_FILENAME = "dictionnary.csv"
 
+        # Select all rows where typedictionnaire is either "sol" or "tec"
         self.cursor.execute("SELECT * FROM tbldictionnaire WHERE typedictionnaire = 'sol' OR typedictionnaire = 'tec'")
         data = self.cursor.fetchall()
         df = pd.DataFrame(data, columns=["id", "codelangue", "typedictionnaire", "codeappelobjet", "indexdictionnaire", "texte"])
+
+        # Remove HTML tags and unescape HTML entities
         df["texte"] = df["texte"].apply(lambda x: re.sub(r"<[^>]*>", "", x))
         df["texte"] = df["texte"].apply(lambda x: unescape(x))
+
+        # Pivot table to have one row per (typedictionnaire, codeappelobjet, codelangue) combination
         merged_df = df.pivot_table(index=["typedictionnaire", "codeappelobjet", "codelangue"], columns="indexdictionnaire", values="texte", aggfunc=lambda x: ' '.join(x))
         merged_df.to_csv(self.output_path + "/" + CSV_FILENAME)
 
     def extract_tblsolution(self):
+        """
+        Extract data from tblsolution and tbltechno tables and save it to a CSV file
+        in order to have a list of numsolution and child codetechno for each codetechno"""
 
         CSV_FILENAME = "techno_solution.csv"
 
+        # Select all rows from tblsolution
         self.cursor.execute("SELECT numsolution, codetechno FROM tblsolution")
         solution_techno = self.cursor.fetchall()
-        df = pd.DataFrame(solution_techno, columns=["numsolution", "codetechno"])
-        df = df.groupby("codetechno")["numsolution"].apply(list).reset_index()
+        df_sol = pd.DataFrame(solution_techno, columns=["numsolution", "codetechno"])
+
+        # Group by codetechno and create a list of numsolution for each codetechno
+        df_sol = df_sol.groupby("codetechno")["numsolution"].apply(list).reset_index()
+
+        # Select all rows from tbltechno
+        self.cursor.execute("SELECT numtechno, codeparenttechno FROM tbltechno")
+        techno_parent = self.cursor.fetchall()
+        df_tech = pd.DataFrame(techno_parent, columns=["codetechno", "codeparenttechno"])
+
+        # Transform the df in order to have one row per codetechno and a list of child codetechno
+        df_tech = df_tech.groupby("codeparenttechno")["codetechno"].apply(list).reset_index()
+        # Rename the columns
+        df_tech.columns = ["codetechno", "child_techno"]
+        # Concatenate the two dataframes
+        df = pd.merge(df_tech, df_sol, on="codetechno", how="outer")
+        # Rename the first column to "numtechno"
+        df.columns = ["numtechno", "child_techno", "numsolution"]
+        # Save the result to a CSV file
         df.to_csv(self.output_path + "/" + CSV_FILENAME, index=False)
 
 
